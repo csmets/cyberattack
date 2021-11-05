bfc63f691ce3c0755a865550ec404d4b2c4755ba
Clyde Smets <clyde.smets@gmail.com>
2021-11-05 11:38:30 +1000

    add lighting and day/night cycle

---
+++ src/env/environment.gd
@@ -0,0 +1,11 @@
+extends CanvasModulate
+
+const NIGHT_COLOR = Color("#091d3a")
+const DAY_COLOR = Color("#ffffff")
+const TIME_SCALE = 0.1
+
+var time = 0
+
+func _process(delta):
+	self.time += delta * TIME_SCALE
+	self.color = NIGHT_COLOR.linear_interpolate(DAY_COLOR, abs(sin(time + 1)) / 2)
