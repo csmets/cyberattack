6e849a042326143b4c1dd576409afcb3e86101d8
Clyde Smets <clyde.smets@gmail.com>
2021-11-09 21:33:42 +1000

    improve player movement

--- src/player/player.gd
+++ src/player/player.gd
@@ -50,23 +50,32 @@ func shoot():
 
 
 func movement():
-	look_at(get_global_mouse_position())
+	if shooting:
+		look_at(get_global_mouse_position())
 	
 	var is_moving = false
 	var movement_direction := Vector2.ZERO
+	var rotate = 0
 	
 	if Input.is_action_pressed("up"):
 		movement_direction.y = -1
+		rotate = -90
 		is_moving = true
 	if Input.is_action_pressed("down"):
 		movement_direction.y = 1
+		rotate = 90
 		is_moving = true
 	if Input.is_action_pressed("left"):
 		movement_direction.x = -1
+		rotate = 180
 		is_moving = true
 	if Input.is_action_pressed("right"):
 		movement_direction.x = 1
+		rotate = 0
 		is_moving = true
+	
+	if is_moving and not shooting:
+		rotation_degrees = rotate
 	
 	movement_direction = movement_direction.normalized()
 	move_and_slide(movement_direction * speed)
